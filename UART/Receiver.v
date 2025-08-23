bc78218c3d275c02770e7d18e2a22ea79454a0a1
Prasanth <162542956+CodeCraftsmanPrasanth@users.noreply.github.com>
2025-08-23 14:01:01 +0530

    Update Receiver.v

--- UART/Receiver.v
+++ UART/Receiver.v
@@ -2,12 +2,17 @@
 module Receiver
     #(parameter Data_length=8,
                 parity_en=1)
-    (input serialdata_in,clk2,rst,
-    input tx_done,parity_type,
+    (input serialdata_in,
+     input clk2,
+     input rst,
+    input tx_done,
+    input parity_type,
     output reg [Data_length-1:0] parallel_dataout,
     output reg error, rx_done,
     output baudraterx);
+    
     baud_rate_RX a1(.clk2(clk2),.rst(rst),.baud_clk_R(baudraterx));
+    
     localparam idle=3'b000,
                start=3'b001,
                data=3'b010,
@@ -20,6 +25,7 @@ module Receiver
     reg [3:0] addrlength;
     reg [3:0] bitlength;
     reg i;
+    
     always @(posedge baudraterx or posedge rst) begin
         if (rst) begin
             ns<=idle;
@@ -35,16 +41,49 @@ module Receiver
         else begin
             case (ns)
                 idle: begin ns<=start; end
-                start: if (~tx_done) begin rx_done<=0;
-                            if (count > 1 & ~i ) begin addrlength[count-2]<=serialdata_in;count<=count-1; end
-                            else if (~serialdata_in) begin bitlength<=addrlength-parity_en-2; ns<= data;count<=0;i<=1; end
-                            else begin ns<= start; end end
-                data: if (count < bitlength-1) begin paralleldata[count]<=serialdata_in;count<=count+1; end
-                      else if (parity_en) begin paralleldata[count]<=serialdata_in;count<=3; ns<=parity; end
-                      else begin paralleldata[count]<=serialdata_in; ns<=stop; end
-                parity: if (~parity_type) begin error<=^{paralleldata,serialdata_in}; ns<=stop; end
-                        else begin error<=~(^{paralleldata,serialdata_in}); ns<=stop; end
-                stop: begin parallel_dataout<=paralleldata; rx_done<=1; ns<=start;count<=5;error<=0; end                      
+                start: if (~tx_done) begin 
+                            rx_done<=0;
+                            if (count > 1 & ~i ) begin 
+                                addrlength[count-2]<=serialdata_in;
+                                count<=count-1; 
+                            end
+                            else if (~serialdata_in) begin 
+                                bitlength<=addrlength-parity_en-2; 
+                                ns<= data;count<=0;
+                                i<=1; 
+                            end
+                            else begin 
+                                ns<= start; 
+                            end 
+                        end
+                data: if (count < bitlength-1) begin 
+                            paralleldata[count]<=serialdata_in;
+                            count<=count+1; 
+                        end
+                      else if (parity_en) begin 
+                              paralleldata[count]<=serialdata_in;
+                              count<=3; 
+                              ns<=parity; 
+                          end
+                      else begin 
+                              paralleldata[count]<=serialdata_in; 
+                              ns<=stop; 
+                      end
+                parity: if (~parity_type) begin 
+                            error<=^{paralleldata,serialdata_in}; 
+                            ns<=stop; 
+                        end
+                        else begin 
+                            error<=~(^{paralleldata,serialdata_in}); 
+                            ns<=stop; 
+                        end
+                stop: begin 
+                        parallel_dataout<=paralleldata; 
+                        rx_done<=1; 
+                        ns<=start;
+                        count<=5;
+                        error<=0; 
+                    end                      
            endcase
        end
     end      
