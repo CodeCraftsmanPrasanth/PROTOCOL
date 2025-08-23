0cdbcb51cd4ec3a6d6be8bcfb5bd8607da8be510
Prasanth <162542956+CodeCraftsmanPrasanth@users.noreply.github.com>
2025-08-23 13:48:15 +0530

    Update Transmitter.v

--- UART/Transmitter.v
+++ UART/Transmitter.v
@@ -5,9 +5,9 @@ module Transmitter
     (
     input [Data_length-1:0] datain,
     input clk1,rst,send,parity_type,
-    output baudrattx,
+    output baudratetx,
     output reg serialdata_out, tx_done);
-    baud_rate_TX a1(.clk1(clk1),.rst(rst),.baud_clk_T(baudrattx));
+    baud_rate_TX a1(.clk1(clk1),.rst(rst),.baud_clk_T(baudratetx));
 
     localparam idle=3'b000,
                frame=3'b001,
@@ -15,32 +15,34 @@ module Transmitter
                data=3'b011,
                parity=3'b100,
                stop=3'b101;
-   reg [2:0] ps,ns;
+   reg [2:0] ns;
    reg [3:0] framegen;
    reg [3:0] datacount;
    reg [2:0] framecount;
-  
-   always @(posedge baudrattx or posedge rst) begin
+   reg  i;
+   always @(posedge baudratetx or posedge rst) begin
     if (rst) begin
         serialdata_out<=1;
         tx_done<=1;
-        ps<=idle;
+        ns<=idle;
         framegen<=0;
         datacount<=0;
-        framecount<=3; end
+        framecount<=3;// 
+        i<=0; end
     else begin
-        case (ps)
-            idle : if (send) begin tx_done<=0; framegen<=Data_length+parity_en+1'b1+1'b1; ps<=frame; end
-                  else begin ps<=idle; end
-            frame : if (framecount>0) begin  serialdata_out<=framegen[framecount]; /*framegen=framegen<<1;*/framecount=framecount-1; end
-                    else begin serialdata_out<=framegen[framecount];ps<=start;framecount<=3 ; end
-            start: begin serialdata_out<=0; ps<=data; end
+        case (ns)
+            idle : if (send) begin  framegen<=Data_length+parity_en+1'b1+1'b1; ns<=frame; end
+                  else begin ns<=idle; end
+            frame : if (framecount>0) begin tx_done<=0; serialdata_out<=framegen[framecount];framecount=framecount-1; end
+                    else begin serialdata_out<=framegen[framecount];ns<=start;framecount<=3 ; end
+            start: if (send) begin tx_done<=0; serialdata_out<=0; ns<=data; end
+                   else if (~i) begin tx_done<=0; serialdata_out<=0; ns<=data; end
             data : if (datacount < Data_length-1 ) begin serialdata_out<=datain[datacount];datacount=datacount+1; end
-                   else if (parity_en) begin serialdata_out<=datain[datacount]; datacount<=0; ps<=parity; end 
-                   else begin serialdata_out<=datain[datacount]; datacount<=0;  ps<=stop; end
-            parity:  if (~parity_type) begin serialdata_out<=^datain; ps<=stop; end
-                     else begin serialdata_out<=~(^datain); ps<=stop; end
-           stop: begin serialdata_out<=1;tx_done<=1;ps<=idle; end
+                   else if (parity_en) begin serialdata_out<=datain[datacount]; datacount<=0; ns<=parity; end 
+                   else begin serialdata_out<=datain[datacount]; datacount<=0;  ns<=stop; end
+            parity:  if (~parity_type) begin serialdata_out<=^datain ; ns<=stop; end
+                     else begin serialdata_out<=~(^datain); ns<=stop; end
+           stop: begin serialdata_out<=1;tx_done<=1;ns<=start;i<=1; end
        endcase
    end 
    end
@@ -61,7 +63,7 @@ module baud_rate_TX(
       baud_clk_T <= 0;
     end
     else begin
-      if(count == clk_div) begin
+      if(count == clk_div-1) begin
       count <= 0;
       baud_clk_T <= 1;
     end
