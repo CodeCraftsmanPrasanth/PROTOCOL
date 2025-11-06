97df2edd37553094025f03f8966830b9d9ed13a3
Prasanth <162542956+CodeCraftsmanPrasanth@users.noreply.github.com>
2025-11-06 16:37:26 +0530

    Update Receiver.v

--- UART/Receiver.v
+++ UART/Receiver.v
@@ -2,16 +2,12 @@
 module Receiver
     #(parameter Data_length=8,
                 parity_en=1)
-    (input serialdata_in,
-     input rst,
-    input tx_done,
-    input parity_type,
+    (input serialdata_in,rst,rx_clk,
+    input tx_done,parity_type,
     output reg [Data_length-1:0] parallel_dataout,
     output reg error, rx_done,
-    output baudraterx,rx_clk);
-    
+    output baudraterx);
     baud_rate_RX a1(.rst(rst),.baud_clk_R(baudraterx),.rx_clk(rx_clk));
-    
     localparam idle=3'b000,
                start=3'b001,
                data=3'b010,
@@ -24,7 +20,6 @@ module Receiver
     reg [3:0] addrlength;
     reg [3:0] bitlength;
     reg i;
-    
     always @(posedge baudraterx or posedge rst) begin
         if (rst) begin
             ns<=idle;
@@ -40,68 +35,30 @@ module Receiver
         else begin
             case (ns)
                 idle: begin ns<=start; end
-                start: if (~tx_done) begin 
-                            rx_done<=0;
-                            if (count > 1 & ~i ) begin 
-                                addrlength[count-2]<=serialdata_in;
-                                count<=count-1; 
-                            end
-                            else if (~serialdata_in) begin 
-                                bitlength<=addrlength-parity_en-2; 
-                                ns<= data;count<=0;
-                                i<=1; 
-                            end
-                            else begin 
-                                ns<= start; 
-                            end 
-                        end
-                data: if (count < bitlength-1) begin 
-                            paralleldata[count]<=serialdata_in;
-                            count<=count+1; 
-                        end
-                      else if (parity_en) begin 
-                              paralleldata[count]<=serialdata_in;
-                              count<=3; 
-                              ns<=parity; 
-                          end
-                      else begin 
-                              paralleldata[count]<=serialdata_in; 
-                              ns<=stop; 
-                      end
-                parity: if (~parity_type) begin 
-                            error<=^{paralleldata,serialdata_in}; 
-                            ns<=stop; 
-                        end
-                        else begin 
-                            error<=~(^{paralleldata,serialdata_in}); 
-                            ns<=stop; 
-                        end
-                stop: begin 
-                        parallel_dataout<=paralleldata; 
-                        rx_done<=1; 
-                        ns<=start;
-                        count<=5;
-                        error<=0; 
-                    end                      
+                start: if (~tx_done) begin rx_done<=0;
+                            if (count > 1 & ~i ) begin addrlength[count-2]<=serialdata_in;count<=count-1; end
+                            else if (~serialdata_in) begin bitlength<=addrlength-parity_en-2; ns<= data;count<=0;i<=1; end
+                            else begin ns<= start; end end
+                data: if (count < bitlength-1) begin paralleldata[count]<=serialdata_in;count<=count+1; end
+                      else if (parity_en) begin paralleldata[count]<=serialdata_in;count<=3; ns<=parity; end
+                      else begin paralleldata[count]<=serialdata_in; ns<=stop; end
+                parity: if (~parity_type) begin error<=^{paralleldata,serialdata_in}; ns<=stop; end
+                        else begin error<=~(^{paralleldata,serialdata_in}); ns<=stop; end
+                stop: begin parallel_dataout<=paralleldata; rx_done<=1; ns<=start;count<=5;error<=0; end                      
            endcase
        end
     end      
 endmodule
 
 module baud_rate_RX(
-  input rst,
-  output reg baud_clk_R, rx_clk);
+  input rst,rx_clk,
+  output reg baud_clk_R );
   parameter integer baud_rate = 9600;
-  parameter integer fqr = 10;
+  parameter integer fqr = 100000000;
   integer count;
   parameter integer clk_per_bit = 10;
-  
-  
-  initial begin
-    rx_clk=0;
-    forever #fqr rx_clk=~rx_clk;
-  end 
-  
+   
+  //fqr/baud_rate
   always@(posedge rx_clk ) begin
     if(rst) begin
       count <= 0;
